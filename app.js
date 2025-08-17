6fed50582c0d060a94e350bfb31527c9cd7dea73
Nitesh Kumar <n14nitesh@gmail.com>
2025-08-18 05:11:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+// let todo =[];
+
+// let req = prompt("please enter your request:");
+ 
+// while(true){
+//     if (req == "quit"){
+//         console.log("quiting the app");
+//         break;
+//     }
+//     if (req == "list"){
+//         console.log(".......................");
+//         for (task of todo){
+//             console.log(task);
+//         }
+//         console.log(".......................")
+
+//     }
+//     else if(req == "add"){
+//         let task = prompt("Please enter a task you want to add");
+//         todo.push(task);
+//         console.log("Task added");
+//     }
+//     else if(req == "delete"){
+//         let task = prompt("please enter the task you want to delete");
+//         let idx = prompt("please enter the task index");
+//         todo.splice(idx, 1);
+//         console.log("Task Deleted")
+//     }
+//     else {
+//         console.log("Wrong Reqiest");
+//     }
+
+//     req = prompt("please enter your reuest");
+
+
+// }
+
+// val1 = Math.floor(Math.random() * 10) + 1;
+// val2 = Math.floor(Math.random() * 10) + 1;
+// console.log(`val1=${val1},val2=${val2}`);
+
+
+// if (val1 < val2){
+//     console.log( val1 *val2);
+// }else if (val1 >= val2){
+//     console.log("I am not able to multiply");
+// }
+
+
+// let val1 = Math.floor(Math.random() * 10) + 1; // random number 1–10
+// let val2 = Math.floor(Math.random() * 10) + 1; // random number 1–10
+
+// console.log(`val1 = ${val1}, val2 = ${val2}`); // show both numbers
+
+// if (val1 < val2) {
+//     console.log(`Multiplication = ${val1 * val2}`);
+// } else {
+//     console.log("I am not able to multiply");
+// }
+
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+// const f = createHelloWorld();
+// console.log(f());        // "Hello World"
+// console.log(f(1, 2, 3)); // "Hello World" (ignores arguments)
+console.log(createHelloWorld()());
+console.log(f(1, 2, 3));
